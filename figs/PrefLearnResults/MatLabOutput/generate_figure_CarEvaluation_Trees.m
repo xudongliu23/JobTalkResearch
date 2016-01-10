da5e1cab30a2c7a941415a7c85e2cfc48277f0e9
Xudong <xudong@xudong-ThinkPad-T410>
2016-01-10 18:47:21 -0500

    Added generate_figure_Wine_Trees.m.

--- figs/PrefLearnResults/MatLabOutput/generate_figure_CarEvaluation_Trees.m
+++ figs/PrefLearnResults/MatLabOutput/generate_figure_CarEvaluation_Trees.m
@@ -25,23 +25,13 @@ fclose(f1ID);
 %hleg1 = legend(C_title{1}{[2,3,4,5]});
 %set(hleg1, 'Location','SouthEast')
 
-
 %% export the figure to pdf
-
 % exportfigure(gcf, '~/test.pdf', [4 3], 300)
 %export_fig ./CarEvaluation_Trees_X_MH.pdf -transparent 
 
 
 
 %% draw CarEvaluation_Trees_MH.pdf
-%fName1 =  'CarEvaluation_Trees.csv';
-%f1ID = fopen(fName1);
-%C_title = textscan(f1ID, '%s', 11, 'Delimiter',',');
-%C_data = textscan(f1ID,'%d %f %f %f %f %f %f %f %f %f %f', 'Delimiter',',');
-%fclose(f1ID);
-% celldisp(C_title)
-% celldisp(C_data)
-
 plot(C_data{1}, C_data{4},  '-r', 'MarkerSize', 6, 'LineWidth', 1.5)
 hold on;
 plot(C_data{1}, C_data{5},  '--r', 'MarkerSize', 6, 'LineWidth', 1.5)
@@ -61,11 +51,6 @@ ylim([0.2 1])
 hleg1 = legend(C_title{1}{[4,5,6,7,8,9,10,11]});
 set(hleg1, 'Location','SouthEast')
 
-
-
-
 %% export the figure to pdf
-
 % exportfigure(gcf, '~/test.pdf', [4 3], 300)
-
 export_fig ./CarEvaluation_Trees_MH.pdf -transparent 
